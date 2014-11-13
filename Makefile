46c60e506c711653bc9cb1982306dfc872a200d4
Kevin Labe <klabe@uchicago.edu>
2014-11-13 12:21:56 -0600

    Replace pgid.c with set -m

--- Makefile
+++
@@ -1,7 +0,0 @@
-all: pgid
-
-pgid: pgid.o
-	g++ -o pgid pgid.o
-
-pgid.o: pgid.c
-	g++ -c pgid.c
--- pgid.c
+++
@@ -1,8 +0,0 @@
-// This is a C function to set the pgid of our processes
-
-#include <unistd.h>
-
-int main(pid_t pid){
-  setpgid(pid,pid);
-  return 0;
-}
